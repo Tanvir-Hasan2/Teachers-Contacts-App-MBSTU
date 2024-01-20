0e18975417fbca3797f891bb06d32b340f8e0271
tanvir-hasan <tanvir8661@gmail.com>
2024-01-20 21:22:36 +0600

    Contact App with Firebase

---
+++ android/app/src/main/kotlin/com/example/teachers_contact_book_mbstu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teachers_contact_book_mbstu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/add_contact_page.dart
@@ -0,0 +1,94 @@
+
+import 'package:flutter/material.dart';
+import 'package:teachers_contact_book_mbstu/controllers/crud_services.dart';
+
+class AddContact extends StatefulWidget {
+  const AddContact({super.key});
+
+  @override
+  State<AddContact> createState() => _AddContactState();
+}
+
+class _AddContactState extends State<AddContact> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("Add Contact",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.white),),
+          centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: TextFormField(
+                      validator: (value) =>
+                          value!.isEmpty ? "Enter any name" : null,
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Name"),
+                      ),
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: TextFormField(
+                      controller: _phoneController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Phone"),
+                      ),
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Email"),
+                      ),
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                    height: 65,
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            CRUDService().addNewContacts(_nameController.text,
+                                _phoneController.text, _emailController.text);
+                            Navigator.pop(context);
+                          }
+                        },
+                        child: Text(
+                          "Create",
+                          style: TextStyle(fontSize: 16),
+                        ))),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
